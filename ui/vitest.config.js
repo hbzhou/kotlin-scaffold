aa4c99233859b4f7ed9db0e577831f4046ca0b6f
Jeremy Zhou <jeremy_zhou@epam.com>
2024-05-30 17:12:28 +0800

    adding vitest v8 coverage & sonar report

---
+++ ui/vitest.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jdom",
+    exclude: ["**/node_modules/**"],
+    threads: false,
+    coverage: {
+      reporter: ["text", "lcov"],
+    },
+    reporters: ["verbose", "junit", "json", "vitest-sonar-reporter"],
+    outputFile: {
+      json: "reports/json-report.json",
+      junit: "test_results/unit_test_report.xml",
+      "vitest-sonar-reporter": "reports/sonar-report.xml",
+    },
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: resolve(__dirname, "src") }],
+    mainFields: ["module", "jsnext:main", "jsnext"],
+  },
+});
